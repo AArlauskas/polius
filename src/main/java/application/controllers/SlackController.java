e32057f249a0d2dc7f35804daf6fc6a998c1fd31
Aurimas <auruxxas.ar@gmail.com>
2019-10-30 14:07:18 +0200

    updates

--- src/main/java/application/controllers/SlackController.java
+++ src/main/java/application/controllers/SlackController.java
@@ -1,39 +1,30 @@
 package application.controllers;
 
-import application.slackJSON.Attachment;
-import application.slackJSON.SlackResponse;
+import application.business.SlackManager;
+import com.github.seratch.jslack.app_backend.views.payload.ViewSubmissionPayload;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import com.github.seratch.jslack.common.json.*;
+import com.google.gson.Gson;
 
 @RestController
 public class SlackController {
-    @RequestMapping(value = "/slack/slash",
-            method = RequestMethod.POST,
-            consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-    public SlackResponse onSlashCommandAccepted(@RequestParam("team_id") String teamId,
-                                                @RequestParam("team_domain") String teamDomain,
-                                                @RequestParam("channel_id") String channelId,
-                                                @RequestParam("channel_name") String channelName,
-                                                @RequestParam("user_id") String userId,
-                                                @RequestParam("user_name") String userName,
-                                                @RequestParam("command") String command,
-                                                @RequestParam("text") String text,
-                                                @RequestParam("response_url") String responseUrl) {
-        SlackResponse response = new SlackResponse();
-        response.setText("Well, it does something");
-        response.setResponseType("in_channel");
+    private SlackManager slackManager;
 
-        Attachment attachment = new Attachment();
-        attachment.setText(" - Pagiriotas");
-        attachment.setColor("#0000ff");
+    public SlackController(){
+        slackManager = new SlackManager();
+    }
 
-        response.addAttachment(attachment);
+    @RequestMapping(value = "/slack/slash", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void onSlashCommandAccepted(@RequestParam("trigger_id") String triggerId) {
+        slackManager.sendInitialModalResponse(triggerId);
+    }
 
-        response.setText(attachment.toString());
+    @RequestMapping(value = "/slack/interact", method = RequestMethod.POST, consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+    public void onInteraction(@RequestParam("payload") String jsonResponse){
+        ViewSubmissionPayload payload = GsonFactory.createSnakeCase().fromJson(jsonResponse, ViewSubmissionPayload.class);
+        System.out.println(payload);
 
-        return response;
     }
 }
